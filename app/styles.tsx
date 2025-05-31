e2e5b7da5d7edf4568f263f2f6115ec7dc4d1cb8
Yaikob/92 <wyaikob@gmail.com>
2025-05-31 11:42:00 +0300

    Add global CSS with Google Fonts imports and vendor style setup

---
+++ app/styles.tsx
@@ -0,0 +1,13 @@
+"use client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "@/public/vendor/bootstrap-icons/bootstrap-icons.css";
+import "@/public/vendor/aos/aos.css";
+import "@/public/vendor/swiper/swiper-bundle.min.css";
+import "@/public/vendor/glightbox/css/glightbox.min.css";
+import "@/public/vendor/drift-zoom/drift-basic.css";
+import "@/public/css/main.css";
+
+export default function Styles() {
+  return null;
+}
